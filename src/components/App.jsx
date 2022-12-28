5765af76d489a545e08cdd458a1d3cec4f42b135
MartyniukOleg91 <111987629+MartyniukOleg91@users.noreply.github.com>
2022-12-28 19:39:36 +0200

    a

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,7 @@ export const App = () => {
         alignItems: 'center',
         fontSize: 40,
         color: '#010101',
-        backgroundColor: 'red'
+     
       }}
     >
       React homework template
